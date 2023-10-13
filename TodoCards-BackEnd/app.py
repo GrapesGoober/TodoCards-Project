2863e855c418c75333b65074c1bc339e627b1604
Graps <twinklierre@gmail.com>
2023-10-13 21:31:47 +0700

    Added template sql query

--- TodoCards-BackEnd/app.py
+++ TodoCards-BackEnd/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -10,6 +11,23 @@ def ping():
     # Check port number if you're getting CORS error!
     response.headers.add('Access-Control-Allow-Origin', 'http://127.0.0.1:5173')
     return response
+
+@app.route("/select-star")
+def select_star():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="test"
+    )
+
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM signup")
+    myresult = mycursor.fetchall()
+
+    response = jsonify(myresult)
+    return response
+
 
 if __name__ == "__main__":
     # default to 127.0.0.1 port 5000
