9aedc7bb602e25d90ce5378fafbb8571f4e4f5c6
Graps <twinklierre@gmail.com>
2023-10-13 20:41:06 +0700

    Init FrontEnd App

---
+++ TodoCards-FrontEnd/src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<h1>This is a setup skeleton project by Grapes</h1>
+
+To run this project
+- cd TodoCard-FrontEnd
+- npm run dev
+- a localhost server should pop up, good to go
+
+<h1>Playing with Demo</h1>
+<p>If you wanna see the demo working of this skeleton project, go <a href="/demo">here</a></p>
+
+<h3>FrontEnd</h3>
+<p>
+    This skeleton template contains a basic frontend, made in svelte.
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+    Check out <a href="https://svelte.dev/examples/reactive-assignments">Examples</a> too!
+    To all codes will be located in "src" folder
+    <li>"lib" contains .js files, i.e. logic and functions for our frontend</li>
+    <li>"src" contains .svelte files, i.e. html-like scripts for displaying stuff</li>
+    <li>every other files are auto generated (i have no idea what they do)</li>
+</p>
+<h3>BackEnd</h3>
+<p>
+    BackEnd will be done in python, using a library "flask". To install, run "pip install Flask".
+
+</p>
---
+++ TodoCards-FrontEnd/src/routes/demo/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	let count = 0;
+
+	function handleClick() {
+		count += 1;
+	}
+</script>
+
+<h1>Click thy button</h1>
+
+<button on:click={handleClick}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
+
+<a href="/">go back</a>
