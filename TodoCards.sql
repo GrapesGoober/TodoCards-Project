8e9b28065c2e29d07c9d004a756c6b87409b84c9
NanoniPck <143884022+NanoniPck@users.noreply.github.com>
2023-10-18 18:35:25 +0700

    TodoCards.sql
    delete subdeckdescription & subdeckDue

--- TodoCards.sql
+++ TodoCards.sql
@@ -2,11 +2,15 @@ DROP DATABASE IF EXISTS TodoCards;
 CREATE DATABASE TodoCards;
 USE TodoCards;
 
+--
+-- Database: `todocards`
+--
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `card`
+--
 
 CREATE TABLE `card` (
   `cardid` int(11) NOT NULL,
@@ -18,13 +22,20 @@ CREATE TABLE `card` (
   `cardcolor` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
+--
+-- Dumping data for table `card`
+--
 
 INSERT INTO `card` (`cardid`, `deckID`, `cardName`, `cardDescription`, `cardDue`, `cardIsFinished`, `cardcolor`) VALUES
 (1, 1, 'Write introduction', '200 words zuzu', '2023-10-17', 0, 'lightblue'),
 (2, 1, 'Write abstract', '500 words zuzu', '2023-11-17', 0, 'pink'),
 (3, 1, 'Cleaning', 'clean bathroom', '2023-11-15', 0, 'lightgreen');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `deck`
+--
 
 CREATE TABLE `deck` (
   `deckid` int(11) NOT NULL,
@@ -32,62 +43,94 @@ CREATE TABLE `deck` (
   `deckDescription` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
+--
+-- Dumping data for table `deck`
+--
 
 INSERT INTO `deck` (`deckid`, `deckName`, `deckDescription`) VALUES
 (1, 'QuickDeck', 'This is the default deck.');
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `subcard`
+--
 
 CREATE TABLE `subcard` (
   `scardid` int(11) NOT NULL,
   `cardID` int(10) NOT NULL,
   `scardName` varchar(50) NOT NULL,
-  `scardDescription` text NOT NULL,
-  `scardDue` date NOT NULL,
   `scardIsFinished` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `subcard`
+--
 
+INSERT INTO `subcard` (`scardid`, `cardID`, `scardName`, `scardIsFinished`) VALUES
+(1, 1, 'write intro', 0),
+(2, 2, 'find motivation(to do this work)', 0);
 
-INSERT INTO `subcard` (`scardid`, `cardID`, `scardName`, `scardDescription`, `scardDue`, `scardIsFinished`) VALUES
-(1, 1, 'write intro', 'make it formal', '2023-11-01', 0),
-(2, 2, 'find motivation(to do this work)', 'just go to temple', '2023-11-01', 0);
+--
+-- Indexes for dumped tables
+--
 
-
+--
+-- Indexes for table `card`
+--
 ALTER TABLE `card`
   ADD PRIMARY KEY (`cardid`),
   ADD KEY `deckID` (`deckID`);
 
-
+--
+-- Indexes for table `deck`
+--
 ALTER TABLE `deck`
   ADD PRIMARY KEY (`deckid`),
   ADD UNIQUE KEY `deckName` (`deckName`);
 
-
+--
+-- Indexes for table `subcard`
+--
 ALTER TABLE `subcard`
   ADD PRIMARY KEY (`scardid`),
   ADD KEY `cardID` (`cardID`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
+--
+-- AUTO_INCREMENT for table `card`
+--
 ALTER TABLE `card`
   MODIFY `cardid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
-
+--
+-- AUTO_INCREMENT for table `deck`
+--
 ALTER TABLE `deck`
   MODIFY `deckid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
-
+--
+-- AUTO_INCREMENT for table `subcard`
+--
 ALTER TABLE `subcard`
   MODIFY `scardid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
+--
+-- Constraints for dumped tables
+--
 
-
-
+--
+-- Constraints for table `card`
+--
 ALTER TABLE `card`
   ADD CONSTRAINT `card_ibfk_1` FOREIGN KEY (`deckID`) REFERENCES `deck` (`deckid`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-
+--
+-- Constraints for table `subcard`
+--
 ALTER TABLE `subcard`
   ADD CONSTRAINT `subcard_ibfk_1` FOREIGN KEY (`cardID`) REFERENCES `card` (`cardid`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
