1bc837f1d48297e8a51576deefb971640f0369b2
Graps <twinklierre@gmail.com>
2023-11-05 14:56:53 +0700

    Implemented the login & signin

--- TodoCards-BackEnd/test.py
+++ TodoCards-BackEnd/test.py
@@ -1,5 +1,18 @@
 import user
 
-result = user.hash_password("password1234")
-print(result)
-print(user.is_password_correct(result, "password1234"))
+# result = user.hash_password("password1234")
+# print(result)
+# print(user.is_password_correct(result, "password1234"))
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="TodoCards"
+)
+
+print(user.signin(mydb, "jenny", "jennypassword"))
+
+print(user.login(mydb, "jenny", "jennypassword"))
