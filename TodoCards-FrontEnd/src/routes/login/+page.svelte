7a7f0cf0186f24f18f9b8c829c7068ec18d6fc3d
awacado <sukmarkpraewa@gmail.com>
2023-11-06 16:23:45 +0700

    skeleton

---
+++ TodoCards-FrontEnd/src/routes/login/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import * as APIs from "$lib"
+
+    let username = "" // the input requires a username & password
+    let password = ""
+    let status = false // status can either be true or false
+    async function login(){
+        status = await APIs.login(username, password)
+    }
+</script>
+
+<input type="text" placeholder="username" bind:value={username}>
+<input type="password" placeholder="password" bind:value={password}>
+<button on:click={login}>submit</button>
---
+++ TodoCards-FrontEnd/src/routes/signup/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import * as APIs from "$lib"
+
+    let signup_username = ""
+    let signup_password = ""
+    let signup_status = false;
+    async function signup(){
+        signup_status = await APIs.signup(signup_username, signup_password)
+    }
+</script>
