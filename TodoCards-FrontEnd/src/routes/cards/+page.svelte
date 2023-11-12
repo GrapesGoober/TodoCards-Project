9f37154114993521bdad82458d9aa57bf558a313
Graps <twinklierre@gmail.com>
2023-11-12 14:57:48 +0700

    Implemented cardslist

--- TodoCards-FrontEnd/src/routes/cards/+page.svelte
+++ TodoCards-FrontEnd/src/routes/cards/+page.svelte
@@ -1,16 +1,39 @@
 <script>
-  import { onMount } from "svelte";
+    import * as APIs from "$lib"
+    import { onMount } from "svelte";
+	import Card from './card.svelte'
 
-    // Need to get the deckId for this page
-    let searchParams
-    let deckId
+    // Send request to backend to query the cards for us
+    let cardslist = []
+    let deckinfo
+    async function getCardslist(){
+        // get the cardslist
+        let searchParams = new URLSearchParams(window.location.search)
+        let deckId = searchParams.get("deckId")
+        cardslist = await APIs.getCardslist(deckId)
 
-    onMount( () => {
-        searchParams = new URLSearchParams(window.location.search)
-        deckId = searchParams.get("deckId")
-    })
+        // also get the deck info to display as well
+        let deckslist = await APIs.getDeckslist()
+        deckinfo = deckslist.find(deck => deck.deckId == deckId)
+        console.log(deckinfo)
+    }
+    onMount(getCardslist)
     
 </script>
 
-<h1>This is the cardslist page</h1>
-<p>The deckid of this page = {deckId}</p>
+
+<!-- Font Awesome 5 Free -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+{#if deckinfo}
+    <h1>{deckinfo.deckName}</h1>
+    <p>{deckinfo.deckDescription}</p>
+{/if}
+
+<button on:click={getCardslist}>refresh</button>
+
+<div>
+    {#each cardslist as card}
+        <Card bind:cardinfo={card} refresh={getCardslist}></Card>
+    {/each}
+</div>
---
+++ TodoCards-FrontEnd/src/routes/cards/card.svelte
@@ -0,0 +1,88 @@
+<script>
+    import * as APIs from "$lib"
+    import { slide } from "svelte/transition";
+    export let cardinfo, refresh
+    let showDescription = false
+    async function finishCard() {
+        let status = await APIs.finishCard(cardinfo.cardId)
+        if (status == true) {
+            refresh()
+        }
+        else {
+            console.log("APIs.finishCard(" + cardinfo.cardId +") returned " + status)
+        }
+    }
+</script>
+
+
+<div class="wrapper">
+
+    {#if cardinfo.isFinished}
+        <div class="card" style="background-color: lightgrey;">
+            <button class="tick" on:click={finishCard}>
+                <i class="fas fa-check-square fa-lg isFinished"></i>
+            </button>
+
+            <button class="title isFinished" on:click={()=>{showDescription = !showDescription}}>
+                {cardinfo.cardName}
+            </button>
+        </div>
+    {:else}
+        <div class="card" style="background-color: {cardinfo.cardColor};">
+            <button class="tick" on:click={finishCard}>
+                <i class="far fa-square fa-lg"></i>
+            </button>
+
+            <button class="title" on:click={()=>{showDescription = !showDescription}}>
+                {cardinfo.cardName}
+            </button>
+        </div>
+    {/if}
+    
+    {#if showDescription}
+        <div class="description" transition:slide>
+            <i class="fas fa-spinner fa-pulse"></i> <br>
+            Due {cardinfo.cardDue} <br>
+            {cardinfo.cardDescription}
+        </div>
+    {/if}
+</div>
+
+
+<style>
+    .wrapper{
+        padding: 10px;
+        width: 20em;
+    }
+    .card {
+        border-radius: 10px;
+        border: none;
+        display: flex;
+        width: 100%;
+    }
+    .title {
+        background-color: transparent;
+        font-size: large;
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        text-align: left;
+        border: none;
+        width: 16em;
+        height: 100%;
+        padding: 10px;
+    }
+    .tick {
+        padding-left: 20px;
+        background-color: transparent;
+        border: none;
+    }
+    .description {
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        font-size: small;
+        width: 20em;
+        padding: 15px;
+    }
+
+    .isFinished {
+        color: grey;
+    }
+</style>
