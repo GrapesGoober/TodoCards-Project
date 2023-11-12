719f9e2fe6982321b7a8810794a7ff7ff855872e
Graps <twinklierre@gmail.com>
2023-11-12 13:13:18 +0700

    Included basic pages

--- TodoCards-FrontEnd/src/routes/+page.svelte
+++ TodoCards-FrontEnd/src/routes/+page.svelte
@@ -1,1 +1,12 @@
+<script>
+
+</script>
+
+<!-- Font Awesome 5 Free -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+
 <h1>This is a skeleton project for TodoCards</h1>
+<h2>This is the deckslist page (the home page)</h2>
+
+
---
+++ TodoCards-FrontEnd/src/routes/cards/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    // Need to get the deckId for this page
+    let searchParams = new URLSearchParams(window.location.search);
+    let deckId = searchParams.get("deckId");
+    
+</script>
+
+<h1>This is the cardslist page</h1>
+<p>The deckid of this page = {deckId}</p>
