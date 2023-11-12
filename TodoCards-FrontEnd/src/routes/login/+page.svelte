bfd4ea2d3096f623703b9868804e7bc5e6095b94
Graps <twinklierre@gmail.com>
2023-11-12 13:29:32 +0700

    Removed status display

--- TodoCards-FrontEnd/src/routes/login/+page.svelte
+++ TodoCards-FrontEnd/src/routes/login/+page.svelte
@@ -4,13 +4,12 @@
     let username = "" // the input requires a username & password
     let password = ""
     let wrong_password_text = ""
-    let status = false
     async function login(){
-        status = await APIs.login(username, password)
+        let status = await APIs.login(username, password)
         // upon successful login, we wanna redirect user to home page 
         if (status == true) {
             // (which is default endpoint)
-            window.location.href = "http://localhost:5173";
+            window.location.href = "/";
         }
         // upon bad login, we might want to display something on the screen
         else {
@@ -19,7 +18,6 @@
     }
 </script>
 
-login status = {status} <br>
 <input type="text" placeholder="username" bind:value={username}>
 <input type="password" placeholder="password" bind:value={password}>
 <button on:click={login}>submit</button>
