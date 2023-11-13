9f15dcb6ae1f12ac761c7fc0839ed86d7d8e7464
awacado <sukmarkpraewa@gmail.com>
2023-11-13 13:59:42 +0700

    login, signup changed

--- TodoCards-FrontEnd/src/routes/login/+page.svelte
+++ TodoCards-FrontEnd/src/routes/login/+page.svelte
@@ -16,26 +16,67 @@
             wrongPassword = true
         }
     }
-
-    function goToSignup() {
-        window.location.href = "/signup";
-    }
 </script>
+<div class="loginpage">
+    <h1>Login</h1>
+    <div>
+        <input type="text" placeholder="Username" bind:value={username}>
+    </div>
+    <div>
+        <input type="password" placeholder="Password" bind:value={password}>
+    </div>
 
-<h1>Login</h1>
-<input type="text" placeholder="username" bind:value={username}>
-<input type="password" placeholder="password" bind:value={password}>
-<button on:click={login}>submit</button>
-{#if wrongPassword}
+    {#if wrongPassword}
     <p class="wrong-text">username or password is incorrect</p>  
-{/if}
-<br>
-<p>If you don't have an account, sign up here</p>
-<button on:click={goToSignup}>Sign Up</button>
+    {/if}
+    <button on:click={login}>Submit</button>
+
+    <p class="signup-txt">If you don't have an account, sign up <a href="/signup">here</a></p>
+</div>
 
 <style>
     @import "../style.css";
+    p {
+        margin: 0;
+    }
+    div {
+        width: 300px;
+        height: 40px;
+    }
+    input {
+        width: 250px;
+        height: 28px;
+        border-radius: 16px;
+        border-width: 1px;
+        padding-left: 16px;
+    }
+    button {
+        width: 268px;
+        height: 30px;
+        border: none;
+        border-radius: 15px;
+        background-color: rgb(90, 90, 255);
+        color: white;
+        font-weight: bold;
+        cursor: pointer;
+    }
+    button:hover {
+        background-color: rgb(111, 111, 255);
+    }
+    button:active {
+        background-color: rgb(129, 129, 255);
+    }
     .wrong-text {
         color: red;
+        font-size: 12px;
+        margin-bottom: 12px;
+    }
+    .signup-txt {
+        color: rgb(152, 152, 152);
+        font-size: 12px;
+        margin-top: 12px;
+    }
+    .loginpage {
+        margin-left: 15px;
     }
 </style>
